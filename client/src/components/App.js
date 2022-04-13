1c5be4a4d4c7eb2bc9ac00082979b69b19802d50
Dolev Hadad <dolev0526@gmail.com>
2022-04-13 07:59:13 +0300

    main

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,21 +4,39 @@ import { UsersProvider } from "../contexts/UsersContext";
 import LogIn from "./LogIn/LogIn";
 import MainPage from "./MainPage/MainPage";
 import SignUp from "./SignUp/SignUp";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#c2b0e2",
+      main: "#337066",
+      dark: "#7d6d99",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#a6d4fa",
+      main: "#90caf9",
+      dark: "#648dae",
+      contrastText: "#000",
+    },
+  },
+});
 
 function App() {
   return (
-    <UsersProvider>
-      <div className="App">
-        <div className="header"></div>
-        <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/main-page/*" element={<MainPage />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </div>
-    </UsersProvider>
+    <ThemeProvider theme={theme}>
+      <UsersProvider>
+        <div className="App">
+          <div className="header"></div>
+          <Routes>
+            <Route path="/" element={<LogIn />} />
+            <Route path="/main-page/*" element={<MainPage />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </div>
+      </UsersProvider>
+    </ThemeProvider>
   );
 }
 
